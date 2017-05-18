b5e319f4a543bb326020eaba602e7c8e8062f314
Raphael Ohanian <raphael.ohanian@telecomnancy.eu>
2017-05-18 17:34:28 +0200

    Finalisation de l'interface graphique (reconnaissance des pixels d'un hexagone et pose du pion)

--- forge/main.c
+++ forge/main.c
@@ -1,8 +1,12 @@
 //Les fichiers d'entete
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "SDL/SDL.h"
 #include "image.c"
+#include "position.c"
+#include "alloc.c"
+#include "ajouter.c"
 
 //25*29
 //Les attributs de l'ecran (640 * 480)
@@ -16,20 +20,34 @@ const int t=120;
 int posx = 0;
 int posy = 0;
 
-
-
 //Les surfaces
 SDL_Surface *plateau = NULL;
 SDL_Surface *background = NULL;
 SDL_Surface *screen = NULL;
-SDL_Surface *case0 =NULL;
+
 
 //La structure d'evenement
 SDL_Event event;
 
-int main( int argc, char* args[] ) {
+//Changement de joueur
+char *changeplayer(char* joueuractuel) {
+	if (strcmp(joueuractuel,"rouge")==0) {
+		return ("bleu");
+	}
+	else {
+		return("rouge");
+	}
+}
 
+//int main( int argc, char* args[] ) {
+int main() {
+char* joueuractuel="rouge";
 int quit = 0;
+int ***pixel = alloc_data(121, 450, 2);
+tab(pixel);
+//pixel[120][430][1]=10;
+//printf("pixel num : %d", pixel[120][430][1]);
+//[121][432][2];
 
 //initialisation de tout les sous-systemes de sdl
 if( SDL_Init( SDL_INIT_EVERYTHING ) == -1 ) {
@@ -61,19 +79,24 @@ apply_surface( 90, 90, plateau, screen );
 if( SDL_Flip( screen ) == -1 ) {
         return 1;
 }
-//71.49
+
 while( quit == 0 ) {
 	//Tant qu'il y a un événement à traiter 
 	while( SDL_PollEvent( &event ) ) {
 		switch(event.type) {
 			//Si l'utilisateur clique
 			case SDL_MOUSEBUTTONUP:
-				//clicx = event.button.x;
-				//clicy = event.button.y;
-				//ajouterpiece(fonctionpos(clix,clicy), p);
+				clicx = event.button.x;
+				clicy = event.button.y;
+				//ajouterpiece(fctpos(clix,clicy), p);
 				//printf("pos : (%d,%d) !!", clicx, clicy);
-				case0 = load_image("rouge.bmp"); 
-				apply_surface(90+49+(t%11)*26+(t/11)*13,90+57+(t/11)*19,case0,screen);
+				//printf("fct pos : case (%d,%d) :) ", pixel[0][0][0], pixel[0][0][1]);
+				//printf("fct pos : case (%d,%d) :) ", pixel[11][0][0], pixel[11][0][1]);
+				//printf("fct pos : case %d :) ", fctpos(clicx,clicy, pixel));
+				//case0 = load_image("rouge.bmp"); 
+				//apply_surface(90+49+(t%11)*26+(t/11)*13,90+57+(t/11)*19,case0,screen);
+				addcase(fctpos(clicx,clicy, pixel), screen, joueuractuel);
+				joueuractuel = changeplayer(joueuractuel);
 				if( SDL_Flip( screen ) == -1 ) {
   				      return 1;
 				}
@@ -91,6 +114,7 @@ while( quit == 0 ) {
 SDL_FreeSurface( plateau );
 SDL_FreeSurface( background );
 
+//free_data(pixel, 121, 432);
 //On quitte sdl
 SDL_Quit();
 }
