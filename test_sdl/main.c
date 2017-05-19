a7fb3d7dd9d6ae9bda79d1bdcd8dd4fcc4721605
Julien Derigny <julien.derigny@telecomnancy.eu>
2017-05-19 11:12:19 +0200

    nettoyage de la racine pour pouvoir commencer à y mettre le projet

--- test_sdl/main.c
+++ test_sdl/main.c
@@ -68,3 +68,20 @@ int main() {
 	printf("Programme terminé correctement.\n");
 	return EXIT_SUCCESS;
 }
+
+
+
+void pause() {
+    int continuer = 1;
+    SDL_Event event;
+ 
+    while (continuer)
+    {
+        SDL_WaitEvent(&event);
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                continuer = 0;
+        }
+    }
+}
--- test_sdl/main.h
+++ test_sdl/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 int maint( int argc, char* args[]);
+void pause();
 
 #endif
